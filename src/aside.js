97da94838d5e0bb55310d3f83060e5a1f8b67303
christian.campos <christian.campos@192-168-1-187>
2023-12-11 15:40:55 +0100

    aside and arrow done

--- src/aside.js
+++ src/aside.js
@@ -34,19 +34,42 @@ class Aside extends HTMLElement {
         }
 
         .button-aside{
-            width:10px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            width:25px;
             height:30px;
-            background-color:blue;
             position:fixed;
             bottom:50%;
             left:245px;
             transition: left 0.5s
         }
 
+        .button-aside:hover{
+            cursor: pointer;
+        }
+
+        .button-aside:hover .arrow-1{
+            border-width: 4px 4px 0 0;
+        }
+
         .button-aside.disabled{
             position:fixed;
             bottom:50%;
             left:10px;
+        }
+
+        .arrow-1 {
+            height: 15px;
+            width: 15px;
+            border: 1px solid grey;
+            border-width: 2px 2px 0 0;
+            transform: rotate(225deg);
+            
+        }
+
+        .arrow-1.rotate{
+            transform: rotate(45deg);
         }
         
       </style>
@@ -58,18 +81,22 @@ class Aside extends HTMLElement {
 
           <slot name="user"></slot>
 
-          <div class = button-aside></div>
+          <div class = button-aside>
+            <div class="arrow-1"></div>
+          </div>
       </div>
       `
 
         let aside = this.shadow.querySelector(".aside");
         let userSlot = this.shadow.querySelector('slot[name="user"]');
         let button = this.shadow.querySelector('.button-aside');
+        let arrow = this.shadow.querySelector('.arrow-1');
 
         button.addEventListener('click', () => {
             userSlot.classList.toggle("none");
             aside.classList.toggle("disabled");
             button.classList.toggle("disabled");
+            arrow.classList.toggle('rotate');
             const customEvent = new CustomEvent('aside-change', {
 
             })
